2615bc67a4ac26814218f739f2f33e7cff310a4d
Markus Olsson <j.markus.olsson@gmail.com>
2022-03-14 21:50:40 +0100

    Only run tests on WIndows

--- test/smoke-test.ts
+++ test/smoke-test.ts
@@ -2,9 +2,11 @@ import { getUserLocale } from '../lib/get-user-locale'
 
 describe('getUserLocale', () => {
   it('works', () => {
-    const locale = getUserLocale()
-    expect(locale).not.toBeUndefined()
-    expect(typeof locale).toBe('string')
-    expect(locale).toMatch(/^[a-z]{2}-[A-Z]{2}$/)
+    if (process.platform === 'win32') {
+      const locale = getUserLocale()
+      expect(locale).not.toBeUndefined()
+      expect(typeof locale).toBe('string')
+      expect(locale).toMatch(/^[a-z]{2}-[A-Z]{2}$/)
+    }
   })
 })
